c3331a41ed33f0653498d83bd979692c84e84d92
jacoposergio <djago32@gmail.com>
2022-08-25 12:48:53 +0200

    jumbotron done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
                    <h3>CURRENT SERIES</h3>
                </div>
                <div class="jumbo-img">
-                   immagine
+                   <img src="{{asset('img/jumbotron.jpg')}}" alt="jumbotron">
                </div>
            </div>
            <div class="comics-list">
